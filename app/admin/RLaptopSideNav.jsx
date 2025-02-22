efa2339a264a8d123666e442572e273764bd1897
VatsalRishabh27 <vatsalrishabh00@gmail.com>
2025-02-23 02:51:44 +0530

    kjk

---
+++ app/admin/RLaptopSideNav.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import DashboardCard from './DashboardCard'
+
+const RLaptopSideNav = () => {
+  return (
+    <div className='w-2/3'>
+      <DashboardCard />
+    </div>
+  )
+}
+
+export default RLaptopSideNav
--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import LeftLaptoSideNav from './LeftLaptoSideNav'
 import MobileAdminNav from './MobileAdminNav'
+import RLaptopSideNav from './RLaptopSideNav'
 
 const page = () => {
   return (
     <div>
     <LeftLaptoSideNav/>   {/* left side admin panel which changes right side component */}
     <MobileAdminNav/>
+    <RLaptopSideNav/>
     </div>
   )
 }
