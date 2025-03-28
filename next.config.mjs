f0f0bbd95fc6fc1b969da4262f4feace00c6a4a0
VatsalRishabh27 <vatsalrishabh00@gmail.com>
2025-03-28 15:59:39 +0530

    hjh

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['blogger.googleusercontent.com' ,"m.media-amazon.com",
-          "assets.ajio.com",
-          "adn-static1.nykaa.com","aroka.in"], // Add the domain here
-      },
-};
-
-export default nextConfig;
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "www.styledivadesignerboutique.com",
+          pathname: "/uploads/**",
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
+  
