e77d0dee2aa0f51222fda7b6e425c5a918c128cb
VatsalRishabh27 <vatsalrishabh00@gmail.com>
2025-03-29 09:54:39 +0530

    df

--- app/api/models/User.js
+++ app/api/models/User.js
@@ -21,7 +21,11 @@ const userSchema = new Schema({
     dpUrl: String, // Profile picture URL
     addresses: [addressSchema], // Array of addresses
     mobileNumber: { type: String, required: true },
-    typeOfUser: { type: String, default: "Customer" }, // Set default value
+    typeOfUser: { 
+        type: String, 
+        enum: ["Customer", "Admin"], // Restrict roles to either Customer or Admin
+        default: "Customer" // Default role is Customer
+    },
 }, { timestamps: true });
 
 const User = mongoose.models.User || mongoose.model('User', userSchema);
